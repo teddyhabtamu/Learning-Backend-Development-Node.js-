b6ffcdfb6e74ce571319f4487705522f1e71e282
teddyhabtamu <tewodroshabtamu29@gmail.com>
2024-10-17 19:11:46 +0300

    Weekday warrior project done

--- 1.0 EJS (embedded javascript)/index.js
+++ 1.0 EJS (embedded javascript)/index.js
@@ -1,1 +1,29 @@
 import express from 'express';
+import ejs from 'ejs';
+
+const app = express();
+const port = 3000;
+const d = new Date();
+let day = d.getDay();
+var dayType = ''
+var advice = ''
+if (day == 0 || day ==6){
+  dayType = 'a weekend'
+  advice = "It is time to relax";
+}
+else{
+  dayType = 'a weekday'
+  advice = "It is time to work hard";
+}
+
+
+app.get ("/", (req, res)=>{
+  console.log(day)
+  res.render("index.ejs", { 
+    dayType: dayType, 
+    advice: advice})
+})
+
+app.listen(port, ()=>{
+  console.log(`Server is running on port ${port} `)
+})
