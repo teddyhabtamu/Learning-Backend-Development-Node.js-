15d6ed216ff72f8a7b1518193b9b2f8c7112422d
teddyhabtamu <tewodroshabtamu29@gmail.com>
2024-10-17 21:01:28 +0300

    Learnt different ejs tags

--- 1.0 EJS (embedded javascript)/index.js
+++ 1.0 EJS (embedded javascript)/index.js
@@ -1,13 +1,14 @@
 import express from 'express';
 import ejs from 'ejs';
 
-const app = express();
+const app = express();   
 const port = 3000;
 const d = new Date();
-let day = d.getDay();
+const day = d.getDay();
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 var dayType = ''
 var advice = ''
-if (day == 0 || day ==6){
+if (day === 0 || day === 6){
   dayType = 'a weekend'
   advice = "It is time to relax";
 }
@@ -21,7 +22,8 @@ app.get ("/", (req, res)=>{
   console.log(day)
   res.render("index.ejs", { 
     dayType: dayType, 
-    advice: advice})
+    advice: advice,
+    totalDays: days})
 })
 
 app.listen(port, ()=>{
